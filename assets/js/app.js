2f83567cc3608477859f1d41da38c1f8f35aa991
Armand Bryant <armandbryant@Armands-MacBook-Pro.local>
2019-06-04 23:44:48 -0700

    adding files

---
+++ assets/js/app.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $('.parallax').parallax();
+    $('.sidenav').sidenav();
+  });
+//Side navbar
+$(document).ready(function(){
+  $('.collapsible').collapsible();
+});
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+});
+$(document).ready(function(){
+    $('.collapsible').collapsible();
+  });
+//Modal trigger
+$(document).ready(function(){
+  $('.modal').modal();
+});  
+//Select
+$(document).ready(function(){
+  $('select').formSelect();
+});
